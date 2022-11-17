c2c2313ac83781787d643848efe5677f8e94d46c
Alberto Parente <albertoparentefh@gmail.com>
2022-11-16 23:20:51 -0300

    Adding migrate +++

--- db/migration/000001_initial_tables.up.sql
+++ db/migration/000001_initial_tables.up.sql
@@ -5,3 +5,29 @@ CREATE TABLE "users"(
     "username" VARCHAR UNIQUE NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT (NOW())
 )
+
+CREATE TABLE "categories" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "user_id" VARCHAR NOT NULL,
+    "title" VARCHAR NOT NULL,
+    "type" VARCHAR NOT NULL,
+    "description" VARCHAR NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT (NOW())
+)
+
+ALTER TABLE "categories" ADD FOREIGH KEY ("user_id") REFERENCES "users" ("id")
+
+CREATE TABLE "accounts" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "user_id" VARCHAR NOT NULL,
+    "category_id" VARCHAR NOT NULL,
+    "title" VARCHAR NOT NULL,
+    "type" VARCHAR NOT NULL,
+    "description" VARCHAR NOT NULL,
+    "value" INTEGER NOT NULL,
+    "date" DATE NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT (NOW())
+)
+
+ALTER TABLE "accounts" ADD FOREIGH KEY ("user_id") REFERENCES "users" ("id")
+ALTER TABLE "accounts" ADD FOREIGH KEY ("category_id") REFERENCES "categories" ("id")
