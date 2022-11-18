00e2f091b6052ce1aab44a5dbe19591d60d26ad2
Alberto Parente <albertoparentefh@gmail.com>
2022-11-17 23:25:21 -0300

    Adding makfile

--- db/migration/000001_initial_tables.up.sql
+++ db/migration/000001_initial_tables.up.sql
@@ -2,32 +2,32 @@ CREATE TABLE "users"(
     "id" SERIAL PRIMARY KEY NOT NULL,
     "username" VARCHAR NOT NULL,
     "password" VARCHAR NOT NULL,
-    "username" VARCHAR UNIQUE NOT NULL,
+    "email" VARCHAR UNIQUE NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT (NOW())
-)
+);
 
 CREATE TABLE "categories" (
     "id" SERIAL PRIMARY KEY NOT NULL,
-    "user_id" VARCHAR NOT NULL,
+    "user_id" INT NOT NULL,
     "title" VARCHAR NOT NULL,
     "type" VARCHAR NOT NULL,
     "description" VARCHAR NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT (NOW())
-)
+);
 
-ALTER TABLE "categories" ADD FOREIGH KEY ("user_id") REFERENCES "users" ("id")
+ALTER TABLE "categories" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
 CREATE TABLE "accounts" (
     "id" SERIAL PRIMARY KEY NOT NULL,
-    "user_id" VARCHAR NOT NULL,
-    "category_id" VARCHAR NOT NULL,
+    "user_id" INT NOT NULL,
+    "category_id" INT NOT NULL,
     "title" VARCHAR NOT NULL,
     "type" VARCHAR NOT NULL,
     "description" VARCHAR NOT NULL,
     "value" INTEGER NOT NULL,
     "date" DATE NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT (NOW())
-)
+);
 
-ALTER TABLE "accounts" ADD FOREIGH KEY ("user_id") REFERENCES "users" ("id")
-ALTER TABLE "accounts" ADD FOREIGH KEY ("category_id") REFERENCES "categories" ("id")
+ALTER TABLE "accounts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "accounts" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
