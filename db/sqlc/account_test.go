2549d54ace544ffc2b9f539c2713e6adf672f05c
Alberto Parente <albertoparentefh@gmail.com>
2022-12-01 21:56:31 -0300

    Adjusting

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -10,14 +10,14 @@ import (
 
 func createRandomAccount(t *testing.T) account {
 	user := createRandomUser(t)
-	arq := CreateaccountParams{
+	arq := CreateAccountParams{
 		UserID:      user.ID,
 		Title:       util.RandomString(12),
 		Type:        "debit",
 		Description: util.RandomString(20),
 	}
 
-	account, err := testQueries.Createaccount(context.Background(), arq)
+	account, err := testQueries.CreateAccount(context.Background(), arq)
 	require.NoError(t, err)
 	require.NotEmpty(t, account)
 
@@ -31,12 +31,12 @@ func createRandomAccount(t *testing.T) account {
 }
 
 func TestCreateAccount(t *testing.T) {
-	createRandomaccount(t)
+	createRandomAccount(t)
 }
 
 func TestGetAccount(t *testing.T) {
-	account1 := createRandomaccount(t)
-	account2, err := testQueries.Getaccount(context.Background(), account1.ID)
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
 	require.NoError(t, err)
 	require.NotEmpty(t, account2)
 
@@ -47,49 +47,7 @@ func TestGetAccount(t *testing.T) {
 }
 
 func TestDeleteAccount(t *testing.T) {
-	account := createRandomaccount(t)
+	account := createRandomAccount(t)
 	err := testQueries.DeleteAccount(context.Background(), account.ID)
 	require.NoError(t, err)
-}
-
-func TestUpdateAccount(t *testing.T) account {
-	account1 := createRandomaccount(t)
-
-	arq := CreateaccountParams{
-		ID:          account1.ID,
-		Title:       util.RandomString(12),
-		Description: util.RandomString(20),
-	}
-
-	account2, err := testQueries.UpdateAccount(context.Background(), arq)
-	require.NoError(t, err)
-	require.NotEmpty(t, account2)
-
-	require.Equal(t, account1.ID, account2.ID)
-	require.Equal(t, arq.Title, account2.Title)
-	require.Equal(t, arq.Description, account2.Description)
-	require.NotEmpty(t, account2.CreatedAt)
-}
-
-func TestListAccount(t *testing.T) account {
-	lasaccount := createRandomaccount(t)
-
-	arq := CreateaccountParams{
-		UserID:      lasaccount.UserID,
-		Type:        lasaccount.Type,
-		Title:       lasaccount.Title,
-		Description: lasaccount.Description,
-	}
-
-	account2, err := testQueries.UpdateAccount(context.Background(), arq)
-	require.NoError(t, err)
-	require.NotEmpty(t, account2)
-
-	for _, account := range accounts {
-		require.Equal(t, lasaccount.ID, account2.ID)
-		require.Equal(t, lasaccount.UserID, account2.UserID)
-		require.Equal(t, lasaccount.Title, account2.Title)
-		require.Equal(t, lasaccount.Description, account2.Description)
-		require.NotEmpty(t, lasaccount.CreatedAt)
-	}
 }
