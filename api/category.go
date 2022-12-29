a0cc136b2890936d0f8d3fcd25b156bd606385bf
Alberto Parente <47580734+AlbertoParente@users.noreply.github.com>
2022-12-29 19:50:45 -0300

    Adjusting

--- api/category.go
+++ api/category.go
@@ -1,137 +1,134 @@
-package api
+//package api
 
-import (
-	"database/sql"
-	"net/http"
+//import (
+//	"database/sql"
+//	"net/http"
 
-	db "github.com/albertoparente/go-finance-app/db/sqlc"
-	"github.com/gin-gonic/gin"
-)
+//	db "github.com/albertoparente/go-finance-app/db/sqlc"
+//	"github.com/gin-gonic/gin"
+//)
 
-type createCategoryRequest struct {
-	UserID      int32  `json:"user_id" binding:"required"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-}
+//type createCategoryRequest struct {
+//	UserID      int32  `json:"user_id" binding:"required"`
+//	Title       string `json:"title"`
+//	Description string `json:"description"`
+//}
 
-func (server *Server) createCategory(ctx *gin.Context) {
-	var req createCategoryRequest
-	err := ctx.ShouldBindJSON(&req)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-	}
+//func (server *Server) createCategory(ctx *gin.Context) {
+//	var req createCategoryRequest
+//	err := ctx.ShouldBindJSON(&req)
+//	if err != nil {
+//		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+//	}
 
-	arg := db.CreateCategoryParams{
-		UserID:      req.UserID,
-		Title:       req.Title,
-		Description: req.Description,
-	}
+//	arg := db.CreateCategoryParams{
+//		UserID:      req.UserID,
+//		Title:       req.Title,
+//		Description: req.Description,
+//	}
 
-	category, err := server.store.CreateCategory(ctx, arg)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-	}
+//	category, err := server.store.CreateCategory(ctx, arg)
+//	if err != nil {
+//		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+//	}
 
-	ctx.JSON(http.StatusOK, category)
-}
+//	ctx.JSON(http.StatusOK, category)
+//}
 
-type getCategoryRequest struct {
-	ID int32 `uri:"id" binding:"required"`
-}
+//type getCategoryRequest struct {
+//	ID int32 `uri:"id" binding:"required"}func (server *Server) GetCategory(ctx *gin.Context) {
+//	var req getCategoryRequest
+//	err := ctx.ShouldBindUri(&req)
+//	if err != nil {
+//		ctx.JSON(http.StatusNotFound, erroResponse(err))
+//	}
 
-func (server *Server) GetCategory(ctx *gin.Context) {
-	var req getCategoryRequest
-	err := ctx.ShouldBindUri(&req)
-	if err != nil {
-		ctx.JSON(http.StatusNotFound, erroResponse(err))
-	}
+//	category, err := server.store.GetCategory(ctx, req.ID)
+//	if err != nil {
+//		if err == sql.ErrNoRows {
+//			ctx.JSON(http.StatusNotFound, erroResponse(err))
+//			return
+//		}
+//		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+//		return
+//	}
 
-	category, err := server.store.GetCategory(ctx, req.ID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusNotFound, erroResponse(err))
-			return
-		}
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
+//	ctx.JSON(http.StatusOK, category)
+//}
 
-	ctx.JSON(http.StatusOK, category)
-}
+//type deleteCategoryRequest struct {
+//	ID int32 `uri:"id" binding:"required"`
+//}
 
-type deleteCategoryRequest struct {
-	ID int32 `uri:"id" binding:"required"`
-}
+//func (server *Server) deleteCategory(ctx *gin.Context) {
+//	var req deleteCategoryRequest
+//	err := ctx.ShouldBindUri(&req)
+//	if err != nil {
+//		ctx.JSON(http.StatusNotFound, erroResponse(err))
+//	}
 
-func (server *Server) deleteCategory(ctx *gin.Context) {
-	var req deleteCategoryRequest
-	err := ctx.ShouldBindUri(&req)
-	if err != nil {
-		ctx.JSON(http.StatusNotFound, erroResponse(err))
-	}
+//	err = server.store.DeleteCategories(ctx, req.ID)
+//	if err != nil {
+//		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+//		return
+//	}
 
-	err = server.store.DeleteCategories(ctx, req.ID)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
+//	ctx.JSON(http.StatusOK, true)
+//}
 
-	ctx.JSON(http.StatusOK, true)
-}
+//type updateCategoryRequest struct {
+//	ID          int32  `json:"id" binding:"required"`
+//	Title       string `json:"title" binding:"required"`
+//	Description string `json:"description" binding:"required"`
+//}
 
-type updateCategoryRequest struct {
-	ID          int32  `json:"id" binding:"required"`
-	Title       string `json:"title" binding:"required"`
-	Description string `json:"description" binding:"required"`
-}
+//func (server *Server) updateCategory(ctx *gin.Context) {
+//	var req updateCategoryRequest
+//	err := ctx.ShouldBindJSON(&req)
+//	if err != nil {
+//		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+//	}
 
-func (server *Server) updateCategory(ctx *gin.Context) {
-	var req updateCategoryRequest
-	err := ctx.ShouldBindJSON(&req)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-	}
+//	arg := db.UpdateCategoryParams{
+//		ID:          req.ID,
+//		Title:       req.Title,
+//		Description: req.Description,
+//	}
 
-	arg := db.UpdateCategoryParams{
-		ID:          req.ID,
-		Title:       req.Title,
-		Description: req.Description,
-	}
+//	category, err := server.store.UpdateCategories(ctx, arg)
+//	if err != nil {
+//		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+//	}
 
-	category, err := server.store.UpdateCategories(ctx, arg)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-	}
+//	ctx.JSON(http.StatusOK, category)
+//}
 
-	ctx.JSON(http.StatusOK, category)
-}
+//type getCategoriesRequest struct {
+//	UserID      int32  `json:"user_id"`
+//	Type        string `json:"type"`
+//	Title       string `json:"title"`
+//	Description string `json:"description"`
+//}
 
-type getCategoriesRequest struct {
-	UserID      int32  `json:"user_id"`
-	Type        string `json:"type"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-}
+//func (server *Server) GetCategory(ctx *gin.Context) {
+//	var req getCategoriesRequest
+//	err := ctx.ShouldBindJSON(&req)
+//	if err != nil {
+//		ctx.JSON(http.StatusNotFound, erroResponse(err))
+//	}
 
-func (server *Server) GetCategory(ctx *gin.Context) {
-	var req getCategoriesRequest
-	err := ctx.ShouldBindJSON(&req)
-	if err != nil {
-		ctx.JSON(http.StatusNotFound, erroResponse(err))
-	}
+//	arg := db.GetCategoriesParams{
+//		UserID:      req.UserID,
+//		Title:       req.Title,
+//		Type:        req.Type,
+//		Description: req.Description,
+//	}
 
-	arg := db.GetCategoriesParams{
-		UserID:      req.UserID,
-		Title:       req.Title,
-		Type:        req.Type,
-		Description: req.Description,
-	}
+//	categories, err := server.store.GetCategories(ctx, arg)
+//	if err != nil {
+//		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+//		return
+//	}
 
-	categories, err := server.store.GetCategories(ctx, arg)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
-
-	ctx.JSON(http.StatusOK, categories)
-}
+//	ctx.JSON(http.StatusOK, categories)
+//}
