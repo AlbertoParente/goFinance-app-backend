131a246c1b7599789a47a460aa231d0df69b9eb7
Alberto Parente <47580734+AlbertoParente@users.noreply.github.com>
2022-12-30 22:36:16 -0300

    Adjusting

--- api/user.go
+++ api/user.go
@@ -1,84 +1,84 @@
-package api
+//package api
 
-import (
-	"database/sql"
-	"net/http"
+//import (
+//	"database/sql"
+//	"net/http"
 
-	db "github.com/AlbertoParente/go-finance-app/db/sqlc"
-	"github.com/gin-gonic/gin"
-)
+//	db "github.com/AlbertoParente/go-finance-app/db/sqlc"
+//	"github.com/gin-gonic/gin"
+//)
 
-type createUserRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email    string `json:"email"`
-}
+//type createUserRequest struct {
+//	Username string `json:"username"`
+//	Password string `json:"password"`
+//	Email    string `json:"email"`
+//}
 
-func (server *Server) createUser(ctx *gin.Context) {
-	var req createUserRequest
-	err := ctx.ShouldBindJSON(&req)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-	}
+//func (server *Server) createUser(ctx *gin.Context) {
+//	var req createUserRequest
+//	err := ctx.ShouldBindJSON(&req)
+//	if err != nil {
+//		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+//	}
 
-	arg := db.CreateUserParams{
-		Username: req.Username,
-		Password: req.Password,
-		Email:    req.Email,
-	}
+//	arg := db.CreateUserParams{
+//		Username: req.Username,
+//		Password: req.Password,
+//		Email:    req.Email,
+//	}
 
-	user, err := server.store.createUser(ctx, arg)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-	}
+//	user, err := server.store.createUser(ctx, arg)
+//	if err != nil {
+//		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+//	}
 
-	ctx.JSON(http.StatusOK, user)
-}
+//	ctx.JSON(http.StatusOK, user)
+//}
 
-type getUserRequest struct {
-	Username string `uri:"username" binding:"required"`
-}
+//type getUserRequest struct {
+//	Username string `uri:"username" binding:"required"`
+//}
 
-func (server *Server) getUser(ctx *gin.Context) {
-	var req getUserRequest
-	err := ctx.ShouldBindUri(&req)
-	if err != nil {
-		ctx.JSON(http.StatusNotFound, erroResponse(err))
-	}
+//func (server *Server) getUser(ctx *gin.Context) {
+//	var req getUserRequest
+//	err := ctx.ShouldBindUri(&req)
+//	if err != nil {
+//		ctx.JSON(http.StatusNotFound, erroResponse(err))
+//	}
 
-	user, err := server.store.getUser(ctx, req.Username)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusNotFound, erroResponse(err))
-			return
-		}
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
+//	user, err := server.store.getUser(ctx, req.Username)
+//	if err != nil {
+//		if err == sql.ErrNoRows {
+//			ctx.JSON(http.StatusNotFound, erroResponse(err))
+//			return
+//		}
+//		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+//		return
+//	}
 
-	ctx.JSON(http.StatusOK, user)
-}
+//	ctx.JSON(http.StatusOK, user)
+//}
 
-type getUserByIdRequest struct {
-	Username int32 `uri:"id" binding:"required"`
-}
+//type getUserByIdRequest struct {
+//	Username int32 `uri:"id" binding:"required"`
+//}
 
-func (server *Server) getUserById(ctx *gin.Context) {
-	var req getUserByIdRequest
-	err := ctx.ShouldBindUri(&req)
-	if err != nil {
-		ctx.JSON(http.StatusNotFound, erroResponse(err))
-	}
+//func (server *Server) getUserById(ctx *gin.Context) {
+//	var req getUserByIdRequest
+//	err := ctx.ShouldBindUri(&req)
+//	if err != nil {
+//		ctx.JSON(http.StatusNotFound, erroResponse(err))
+//	}
 
-	user, err := server.store.getUserById(ctx, req.ID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			ctx.JSON(http.StatusNotFound, erroResponse(err))
-			return
-		}
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
+//	user, err := server.store.getUserById(ctx, req.ID)
+//	if err != nil {
+//		if err == sql.ErrNoRows {
+//			ctx.JSON(http.StatusNotFound, erroResponse(err))
+//			return
+//		}
+//		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+//		return
+//	}
 
-	ctx.JSON(http.StatusOK, user)
-}
+//	ctx.JSON(http.StatusOK, user)
+//}
