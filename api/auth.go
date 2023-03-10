d3ee256249c1bf03a2cea880157c00045b8699da
albertoparente <albertoparentefh@gmail.com>
2023-03-09 23:20:01 -0300

    Adding ++-

--- api/auth.go
+++ api/auth.go
@@ -1,6 +1,16 @@
 package api
 
-import "github.com/golang-jwt/jwt/v4"
+import (
+	"bytes"
+	"crypto/sha512"
+	"database/sql"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
+)
 
 // "bytes"
 // "crypto/sha512"
@@ -22,50 +32,50 @@ type Claims struct {
 	jwt.RegisteredClaims
 }
 
-// func (server *Server) login(ctx *gin.Context) {
-// 	var req loginRequest
-// 	err := ctx.ShouldBindJSON(&req)
-// 	if err != nil {
-// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-// 	}
+func (server *Server) login(ctx *gin.Context) {
+	var req loginRequest
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
 
-// 	user, err := server.store.GetUser(ctx, req.Username)
-// 	if err != nil {
-// 		if err == sql.ErrNoRows {
-// 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-// 			return
-// 		}
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
-// 	hashedInput := sha512.Sum512_256([]byte(req.Password))
-// 	trimmedHash := bytes.Trim(hashedInput[:], "\x00")
-// 	preparedPassword := string(trimmedHash)
-// 	plainTextInBytes := []byte(preparedPassword)
-// 	hashTextInBytes := []byte(user.Password)
-// 	err = bcrypt.CompareHashAndPassword(hashTextInBytes, plainTextInBytes)
-// 	if err != nil {
-// 		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
-// 		return
-// 	}
+	hashedInput := sha512.Sum512_256([]byte(req.Password))
+	trimmedHash := bytes.Trim(hashedInput[:], "\x00")
+	preparedPassword := string(trimmedHash)
+	plainTextInBytes := []byte(preparedPassword)
+	hashTextInBytes := []byte(user.Password)
+	err = bcrypt.CompareHashAndPassword(hashTextInBytes, plainTextInBytes)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
 
-// 	expirationTime := time.Now().Add(100 * time.Minute)
+	expirationTime := time.Now().Add(100 * time.Minute)
 
-// 	claims := &Claims{
-// 		Username: req.Username,
-// 		RegisteredClaims: jwt.RegisteredClaims{
-// 			ExpiresAt: jwt.NewNumericDate(expirationTime),
-// 		},
-// 	}
+	claims := &Claims{
+		Username: req.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
 
-// 	generatedToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-// 	var jwtSignedKey = []byte("secret_key")
-// 	generatedTokenToString, err := generatedToken.SignedString(jwtSignedKey)
-// 	if err != nil {
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
+	generatedToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	var jwtSignedKey = []byte("secret_key")
+	generatedTokenToString, err := generatedToken.SignedString(jwtSignedKey)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
-// 	ctx.JSON(http.StatusOK, generatedTokenToString)
-// }
+	ctx.JSON(http.StatusOK, generatedTokenToString)
+}
